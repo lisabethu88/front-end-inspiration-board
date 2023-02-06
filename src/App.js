682d847ba684b32ed4d034e8d5af5a3ac7c36287
Xuan Hien Pham <phamxuanhien02@gmail.com>
2023-02-06 12:27:12 -0500

    created board branch,added code to NewBoardForm.js to create a new board

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import './App.css';
+import "./App.css";
 import { useState, useEffect } from "react";
-import BoardList from './components/BoardList';
-import NewBoardForm from './components/NewBoardForm';
+import Board from "./components/Board";
+import NewBoardForm from "./components/NewBoardForm";
 import axios from "axios";
 
 const kBaseUrl = process.env.REACT_APP_BACKEND_URL;
 
 function App() {
+  const [boardState, setBoardState] = useState([]);
+
+  /*
+  const createNewBoard = (formData) => {};
+  */
+
   return (
     <main>
-      <NewBoardForm></NewBoardForm>
-      <BoardList></BoardList>
+      <NewBoardForm createNewBoard={createNewBoard}></NewBoardForm>
+      <Board board={boardState} onBoardSelect={onBoardSelect}></Board>
     </main>
   );
 }
--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,14 +1,64 @@
 import React, { useState } from "react";
 
-const NewBoardForm = () => {
-    return (
-        <section>
-            <form>
-                
-            </form>
-        </section>
-    )
+const kDefaultFormState = {
+  title: "",
+  ownerName: "",
+};
 
+const NewBoardForm = ({ createNewBoard }) => {
+  const [formData, setFormData] = useState(kDefaultFormState);
+
+  const handleChange = (event) => {
+    const fieldName = event.target.name;
+    const fieldValue = event.target.value;
+
+    const newFormData = {
+      ...formData,
+      [fieldName]: fieldValue,
+    };
+
+    setFormData(newFormData);
+  };
+
+  const formSubmit = (event) => {
+    event.preventDefault();
+    createNewBoard(formData);
+    setFormData(kDefaultFormState);
+  };
+
+  return (
+    <section>
+      <h2>Create a New Board</h2>
+      <form onSubmit={formSubmit}>
+        <div>
+          <label>Title</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.title}
+            onChange={handleChange}
+          ></input>
+        </div>
+        <div>
+          <label>Owner's Name</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.ownerName}
+            onChange={handleChange}
+          ></input>
+        </div>
+        <div>
+          <p>
+            Preview: {formData.title} - {formData.ownerName}
+          </p>
+        </div>
+        <div>
+          <input type="submit" value="Submit"></input>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default NewBoardForm;
