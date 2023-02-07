10520dc16200ab38503b85a8d5d16194057b13f3
Lisa Utsett <lisabethutsett@gmail.com>
2023-02-07 12:58:43 -0500

    fixed submit board error

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,7 +2,6 @@ import { React, useState } from "react";
 import "./Board.css";
 import PropTypes from "prop-types";
 
-
 const Board = ({ board, selectBoard }) => {
     return (
         <p className="board-list-item" onClick={()=>selectBoard(board)}> 
